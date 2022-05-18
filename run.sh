83c6833e0025942aa43715b657817f261b85ad30
Puneeth Rai <puneeth.rai@intel.com>
2022-05-18 13:06:09 +0530

    mod probe fuse

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ echo $(printenv)
 echo $(ls -lrt /s3fs-fuse)
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
+modprobe fuse
 s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
