45effd856d49db93be0b15c96146fd5e04546888
Puneeth Rai <puneeth.rai@intel.com>
2022-05-18 12:36:20 +0530

    prints

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 set -e
 
 echo $(pwd)
+echo $(printenv)
+echo $(ls -lrt /s3fs-fuse)
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
 s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
